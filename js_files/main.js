0deca20600f51d975aff0c347dae72d94a0299e2
Michael Paglia <michaelpaglia02@gmail.com>
2024-03-07 22:04:24 -0500

    edit projects

--- js_files/main.js
+++ js_files/main.js
@@ -1,38 +1,47 @@
 $(document).ready(function() {
-   let $btns = $('.project-area .button-group button');
+    let $btns = $('.project-area .button-group button');
 
-   $btns.click(function(e) {
-    $('.project-area .button-group buttons').removeClass('active');
-    e.target.classList.add('active');
-    let selector = $(e.target).attr('data-filter');
-    $('.project-area .grid').isotope({
-        filter : selector
+    $btns.on('click', function(e) {
+        $('.project-area .button-group button').removeClass('active');
+        $(e.target).addClass('active');
+
+        let filterValue = $(e.target).attr('data-filter');
+        if (filterValue === '*') {
+            $('.project-area .list-group-item').show();
+        } else {
+            $('.project-area .list-group-item').each(function() {
+                let item = $(this);
+                if (item.hasClass(filterValue.substring(1))) {
+                    item.show();
+                } else {
+                    item.hide();
+                }
+            });
+        }
+
+        return false; // Prevent default action
     });
 
-    return false;
-   })
-   
-   $('.project-area.button-group #btn1').trigger('click');
+    // Trigger click on 'All' button to show all projects on page load
+    $('.project-area .button-group #btn1').trigger('click');
 
-   $('.project-area .grid .test-popup-link').magnificPopup({
-    type: 'image',
-    gallery: { enabled: true }
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
     });
-    
-    // Sticky navigation
+
     let nav_offset_top = $('.header_area').height() + 50;
     function navFixed() {
-        if($('.header_area').length) {
+        if ($('.header_area').length) {
             $(window).scroll(function() {
                 let scroll = $(window).scrollTop();
-                if(scroll >= nav_offset_top) {
+                if (scroll >= nav_offset_top) {
                     $('.header_area .main-menu').addClass('navFixed');
                 } else {
                     $('.header_area .main-menu').removeClass('navFixed');
                 }
-            })
+            });
         }
     }
-
     navFixed();
 });
